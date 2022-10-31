4af7ce6a3f361ffd1313e9775ad38deac5831c37
Magnus Hermansen <mauh@itu.dk>
2022-10-31 16:55:29 +0100

    Changed to use CommonlibSSE-NG

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,55 @@
 #include "functions.h"
 
-extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
-	SKSE::PluginVersionData v{};
-	v.pluginVersion = Version::MAJOR;
-	v.PluginName(Version::NAME);
-	v.AuthorName("aers"sv);
-	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-	v.UsesAddressLibrary(true);
-	v.UsesStructsPost629(true);
-	return v;
-}();
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace SKSE::stl;
 
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+
+/**
+    * Setup logging.
+    *
+    * <p>
+    * Logging is important to track issues. CommonLibSSE bundles functionality for spdlog, a common C++ logging
+    * framework. Here we initialize it, using values from the configuration file. This includes support for a debug
+    * logger that shows output in your IDE when it has a debugger attached to Skyrim, as well as a file logger which
+    * writes data to the standard SKSE logging directory at <code>Documents/My Games/Skyrim Special Edition/SKSE</code>
+    * (or <code>Skyrim VR</code> if you are using VR).
+    * </p>
+    */
+void InitializeLogging()
 {
-#ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-	auto path = logger::log_directory();
+	auto path = log_directory();
 	if (!path) {
-		return false;
+		report_and_fail("Unable to lookup SKSE logs directory.");
+	}
+	*path /= PluginDeclaration::GetSingleton()->GetName();
+	*path += L".log";
+
+	std::shared_ptr<spdlog::logger> log;
+	if (IsDebuggerPresent()) {
+		log = std::make_shared<spdlog::logger>(
+			"Global", std::make_shared<spdlog::sinks::msvc_sink_mt>());
+	} else {
+		log = std::make_shared<spdlog::logger>(
+			"Global", std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true));
 	}
 
-	*path /= Version::PROJECT;
-	*path += ".log"sv;
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-#ifndef NDEBUG
-	log->set_level(spdlog::level::trace);
-#else
-	log->set_level(spdlog::level::info);
-	log->flush_on(spdlog::level::warn);
-#endif
+	log->set_level({ spdlog::level::level_enum::info });
+	log->flush_on({ spdlog::level::level_enum::trace });
 
 	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+	spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
+}
 
-	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
-	
-	SKSE::Init(a_skse);
+SKSEPluginLoad(const LoadInterface* skse)
+{
+	InitializeLogging();
 
-	isLoaded = true;
-	
-	logger::info("loaded");
+	auto* plugin = PluginDeclaration::GetSingleton();
+	auto version = plugin->GetVersion();
+	log::info("{} {} is loading...", plugin->GetName(), version);
 
+
+	log::info("{} has finished loading.", plugin->GetName());
 	return true;
 }
